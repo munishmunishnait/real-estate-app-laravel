e153a820e0cd7406f710d4e03dd033d27ebe1e96
Munish <Parasbhagat63@gmail.com>
2023-12-22 04:03:19 -0700

    real-estate-app

---
+++ larazillow/app/Http/Controllers/UserAccountController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Auth\Events\Registered;
+
+class UserAccountController extends Controller
+{
+    public function create(){
+        return inertia('UserAccount/Create');
+    }
+
+    public function store(Request $request){
+
+       $user = User::create($request->validate([
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:8|confirmed'
+       ]));
+    //    $user->save();
+       Auth::login($user);
+       event(new Registered($user));
+
+       return redirect()->route('listing.index')->with('success','Account Created!');
+
+    }
+
+
+}
---
+++ larazillow/resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        You need to be verified first to see this page!
+    </div>
+    <div>
+        <Link :href="route('verification.send')" method="post" as="button" class="text-indigo-600 dark:text-indigo-400">Resend Verification Email</Link>
+    </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+
+
+</script>
